1f57f3a013bf95ffa738a069f48377b35e648a2b
Sebastian Birchner <sebastian.birchner@gmx.de>
2022-08-10 07:07:57 +0200

    Initial Commit

---
+++ MineSweeperSolver/MineSweeperSolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  MineSweeperSolverApp.swift
+//  MineSweeperSolver
+//
+//  Created by Sebastian Birchner on 10.08.22.
+//
+
+import SwiftUI
+
+@main
+struct MineSweeperSolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
